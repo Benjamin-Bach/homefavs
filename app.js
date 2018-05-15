326dbaa6c38f52fbff2ea7f9b4a4f0ca410f37fb
ben <ben@E480>
2018-05-15 14:47:03 +0200

    add icon replacement method, improve design

--- app.js
+++ app.js
@@ -11,11 +11,24 @@ queryDatas.send();
 function parseResp(resp){
   let favs = resp.split(/\n/g);
   let favsCleaned = [];
-  for (i = 0; i < favs.length; i = (i+3) ) {
-    favsCleaned.push({
-      'title' : favs[i],
-      'url' : favs[(i+1)]
-    });
+  for (i = 0; i + 1 < favs.length; i = (i+3) ) {
+
+
+      let urlArray = favs[(i+1)].split('/');
+      let siteName = '';
+      if(urlArray[2]!= 'feedproxy.google.com'){
+        siteName = urlArray[2];
+      }else{
+        siteName = urlArray[4];
+      }
+      console.log(siteName);
+      favsCleaned.push({
+        'title' : favs[i],
+        'url' : favs[(i+1)],
+        'sitename' : siteName
+      });
+
+
   }
   createHtml(favsCleaned);
 }
@@ -28,15 +41,22 @@ function createHtml(fc){
       newItem += '<a href="';
       newItem += fc[i].url;
       newItem += '">';
-      newItem += '<div><img src="' + fetchIcon(fc[i].url) + '" /></div>';
-      newItem += '<p>' + fc[i].title + '</p>';
+      newItem += '<div class="img"><img id="img_' + i + '" src="icon.svg" /></div>';
+      newItem += '<div class="text"><p class="site-name">' + fc[i].sitename + '</p><p>' + fc[i].title + '</p></div>';
       newItem += '</a>';
       main.insertAdjacentHTML('beforeend',newItem);
+      fetchIcon(i, fc[i].url);
     }
   }
 }
 
-function fetchIcon(url){
+function fetchIcon(i, url){
   let urlArray = url.split('/');
-  return urlArray[0] + '//' + urlArray[2] + '/favicon.ico';
+  let urlFavicon = urlArray[0] + '//' + urlArray[2] + '/favicon.ico';
+  let img = new Image();
+  img.onload = function(){
+    let selector = '#img_' + i;
+    document.querySelector(selector).src = urlFavicon;
+  }
+  img.src = urlFavicon;
 }
